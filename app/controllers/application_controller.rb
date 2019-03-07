e0fdb9cef259e3d8abc4565196d683b0746c516f
buddylee939 <buddylee939@hotmail.com>
2019-03-07 02:22:23 -0500

    Updated links on show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  # def after_sign_in_path_for(current_user)
+  #   if current_user.has_roles?(:site_admin, :site_manager)
+  #     admin_dashboard_path
+  #   else
+  #     dashboard_path
+  #   end
+  # end
 end
